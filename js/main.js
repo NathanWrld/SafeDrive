0cee1f9db8da63d5da3be1d842b359b3fa9a39f6
Jonathan Fuenmayor <jonathanfcb.jf@gmail.com>
2026-01-12 20:49:56 -0400

    tratando de arreglar bucle infinito

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 import { checkUserSession, listenAuthChanges } from './auth/auth.js';
 import { initControls } from './ui/controls.js';
+import { supabase } from './config/supabase.js';
 
 const page = document.body.id;
 
-// Siempre
-checkUserSession();
-listenAuthChanges();
-
-// Solo en home
 if (page === 'home') {
+    // Solo en páginas protegidas
+    checkUserSession();      
+    listenAuthChanges();
     initControls();
+} else if (page === 'login') {
+    // Solo en login
+    supabase.auth.getSession().then(({ data: { session } }) => {
+        if (session?.user) {
+            // Si ya está logueado, ir a home
+            window.location.href = 'home.html';
+        }
+    });
 }
 
